606cc7e221304cc0b26f460b44ffe082223f482c
Macstar1 <maopa@bk.ru>
2024-05-01 14:30:49 +0300

    KS-1-2 add number of employees

---
+++ src/main/kotlin/Lesson1/lesson_1_task_2.kt
@@ -0,0 +1,18 @@
+package org.example.Lesson1
+
+fun main() {
+    println("Это решение задачи 2 из урока 1")
+
+    val numberOfOrders: Int = 75
+    val thanksFor: String = "Шикарно!"
+    println("Количество заказов: $numberOfOrders\nБлагодарности: $thanksFor")
+
+    var numberOfEmployees: Int = 2000
+    //println("Количество работников: $numberOfEmployees")
+    numberOfEmployees = 1999
+    println("Количество работников: $numberOfEmployees")
+
+
+
+
+}
