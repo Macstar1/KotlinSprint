3b593336cfc7fa88a69ceced3338770d7d939fb6
Macstar1 <maopa@bk.ru>
2024-05-01 15:15:11 +0300

    KS-1-3 first fly of Gagarin

---
+++ src/main/kotlin/Lesson1/lesson_1_task_3.kt
@@ -0,0 +1,20 @@
+package org.example.Lesson1
+
+fun main(){
+    val firstFlyYear = 1961
+    var firstFlyHour = "09"
+    var firstFlyMinute = "07"
+
+    println("$firstFlyYear\n$firstFlyHour\n$firstFlyMinute")
+
+    firstFlyHour = "10"
+    firstFlyMinute = "55"
+
+    println("$firstFlyHour:$firstFlyMinute")
+
+    //В принципе, можно и так, судя по подсказке:
+    //print(firstFlyHour)
+    //print(":")
+    //print(firstFlyMinute)
+
+}
