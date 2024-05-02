6d998dae0857be352e30b06589af40773f3e2175
Macstar1 <maopa@bk.ru>
2024-05-02 14:52:14 +0300

    KS-1-5 translate seconds to minutes and hours

--- src/main/kotlin/Lesson1/lesson_1_task_5.kt
+++ src/main/kotlin/Lesson1/lesson_1_task_5.kt
@@ -2,15 +2,13 @@ package org.example.Lesson1
 
 fun main(){
     var seconds  = 6480
-    var minutes  = seconds / 60
-    val hours = minutes / 60
+    var minutes  = seconds / PART_OF_TIME
+    val hours = minutes / PART_OF_TIME
 
-    seconds %= 60
-    minutes %= 60
+    seconds %= PART_OF_TIME
+    minutes %= PART_OF_TIME
 
-    val finalSeconds = String.format("%02d", seconds)
-    val finalMinutes = String.format("%02d", minutes)
-
-    println("$hours:$finalMinutes:$finalSeconds")
+    println("%02d:%02d:%02d".format(hours, minutes, seconds))
 
 }
+const val PART_OF_TIME = 60
