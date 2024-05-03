91eb8ee4217513c6c3d6c76ee346ce0a8e15af95
Macstar1 <maopa@bk.ru>
2024-05-03 12:37:58 +0300

    KS-3-1 hello task

---
+++ src/main/kotlin/Lesson3/lesson_3_task_1.kt
@@ -0,0 +1,14 @@
+package org.example.Lesson3
+
+fun main() {
+
+    var hello = "Добрый день, "
+    val name = "Дмитрий"
+    val exclamation = '!'
+
+    println("$hello$name$exclamation")
+
+    hello = "Добрый вечер, "
+
+    println("$hello$name$exclamation")
+}
