5ef49b43fbe20403f89ec768df321e89e436d935
Macstar1 <maopa@bk.ru>
2024-05-03 11:44:07 +0300

    KS-2-3 end of route

--- src/main/kotlin/Lesson2/lesson_2_task_3.kt
+++ src/main/kotlin/Lesson2/lesson_2_task_3.kt
@@ -1,15 +1,16 @@
 package org.example.Lesson2
 
-fun main(){
+fun main() {
     val startHour = 9
     val startMinute = 39
     val routeTime = 457
 
-    val finishTime = startHour * PART_OF_TIME + startMinute + routeTime // Время прибытия в минутах
-    val finishHour = finishTime / PART_OF_TIME //Прибытие часы
-    val finishMinute = finishTime % PART_OF_TIME //Прибытие минуты
+    val finishTime = startHour * PART_OF_TIME + startMinute + routeTime
+    val finishHour = finishTime / PART_OF_TIME
+    val finishMinute = finishTime % PART_OF_TIME
 
     println("Время прибытия: $finishHour:$finishMinute")
 
 }
+
 const val PART_OF_TIME = 60
