5233510b024024028ec5716ac4bd465d2777c6ee
Macstar1 <maopa@bk.ru>
2024-05-03 10:28:50 +0300

    KS-2-4 buff

--- src/main/kotlin/Lesson2/lesson_2_task_1.kt
+++ src/main/kotlin/Lesson2/lesson_2_task_1.kt
@@ -1,18 +1,13 @@
 package org.example.Lesson2
 
-fun main(){
-    //Инициализируем переменные
+fun main() {
     val mike = 3
     val nik = 4
     val tod = 3
     val sergio = 5
     val numberOfStudents = 4
-    val average : Float
+    val average = (((mike + nik + tod + sergio).toFloat()) / numberOfStudents)
 
-    //Вычисляем среднее
-    average = (((mike + nik + tod + sergio).toFloat())/numberOfStudents)
-
-    //Печатаем результат с обязательным выведением двух знаков после запятой
-    println(String.format("Средний балл: %.2f",average))
+    println(String.format("Средний балл: %.2f", average))
 
 }
