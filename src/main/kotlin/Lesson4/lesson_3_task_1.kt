147f72d2b34d6cc1bbda833a4c49d41db55d8863
Macstar1 <maopa@bk.ru>
2024-05-04 15:25:20 +0300

    KS-4-1 free tables

---
+++ src/main/kotlin/Lesson4/lesson_3_task_1.kt
@@ -0,0 +1,11 @@
+package org.example.Lesson4
+
+fun main() {
+    val freeTablesToday = 0
+    val freeTablesTomorrow = 4
+
+    println("Доступность столиков на сегодня: ${freeTablesToday > 0}\nДоступность столиков на завтра: ${freeTablesTomorrow > 0}")
+
+}
+
+const val NUMBER_OF_TABLES = 13
