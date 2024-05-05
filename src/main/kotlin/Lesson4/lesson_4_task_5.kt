e4714c62b64a0beec377bf5c6ad3b47e501f86d5
Macstar1 <maopa@bk.ru>
2024-05-05 16:51:59 +0300

    KS-5-2 age of majority

--- src/main/kotlin/Lesson4/lesson_4_task_5.kt
+++ src/main/kotlin/Lesson4/lesson_4_task_5.kt
@@ -2,20 +2,21 @@ package org.example.Lesson4
 
 fun main() {
     print("Корабль имеет повреждения? (true/false): ")
-    val shipDamage = readLine().toBoolean()
+    val isShipDamage = readln().toBoolean()
     print("Текущий состав экипажа: ")
-    val shipCrew = readLine()?.toInt()
+    val shipCrew = readln().toInt()
     print("Количество ящиков с провизией: ")
-    val food = readLine()?.toInt()
+    val food = readln().toInt()
     print("Метеоусловия благоприятны? (true/false): ")
-    val weather = readLine().toBoolean()
+    val isWeatherGood = readln().toBoolean()
     val shipIsReady =
-        (!shipDamage && (MIN_CRUE_VALUE <= shipCrew!!) && (shipCrew <= MAX_CRUE_VALUE) && (food!! > FOOD) && weather) || (shipDamage && (shipCrew == MAX_CRUE_VALUE) && weather && (food!! >= FOOD))
+        (!isShipDamage && (MIN_CREW_VALUE <= shipCrew) && (shipCrew <= MAX_CREW_VALUE) && (food > FOOD) &&
+                isWeatherGood) || (isShipDamage && (shipCrew == MAX_CREW_VALUE) && isWeatherGood && (food >= FOOD))
 
     println("Готовность корабля: $shipIsReady")
 
 }
 
-const val MAX_CRUE_VALUE = 70
-const val MIN_CRUE_VALUE = 55
+const val MAX_CREW_VALUE = 70
+const val MIN_CREW_VALUE = 55
 const val FOOD = 50
