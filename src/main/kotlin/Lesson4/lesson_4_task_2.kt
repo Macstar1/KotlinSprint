a70ff0f840f643f2a3e5b83963423239ce616a80
Macstar1 <maopa@bk.ru>
2024-05-05 14:35:24 +0300

    KS-5-2 age of majority

--- src/main/kotlin/Lesson4/lesson_4_task_2.kt
+++ src/main/kotlin/Lesson4/lesson_4_task_2.kt
@@ -4,12 +4,20 @@ fun main() {
     var weight = 20
     var volume = 80
 
-    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${weight > MIN_WEIGHT && weight < MAX_WEIGHT && volume < MAX_VOLUME}")
+    println(
+        "Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${
+            weight > MIN_WEIGHT && weight < MAX_WEIGHT && volume < MAX_VOLUME
+        }"
+    )
 
     weight = 50
     volume = 100
 
-    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${weight > MIN_WEIGHT && weight < MAX_WEIGHT && volume < MAX_VOLUME}")
+    println(
+        "Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${
+            weight > MIN_WEIGHT && weight < MAX_WEIGHT && volume < MAX_VOLUME
+        }"
+    )
 
 }
 
