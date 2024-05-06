04540265ea609d509002dbebffacb7cf997afbd0
Macstar1 <maopa@bk.ru>
2024-05-06 06:48:55 +0300

    KS-5-5 lottery

--- src/main/kotlin/Lesson5/lesson_5_task_5.kt
+++ src/main/kotlin/Lesson5/lesson_5_task_5.kt
@@ -4,12 +4,14 @@ import kotlin.random.Random
 
 fun main() {
     val randomValues = listOf(Random.nextInt(0, 42), Random.nextInt(0, 42), Random.nextInt(0, 42))
+
     // Для проверки работоспособности программы
     // println(randomValues)
 
     val userValues = mutableListOf(readln().toInt(), readln().toInt(), readln().toInt())
     val intersectList = userValues.intersect(randomValues.toList())
     val numberOfIntersection = intersectList.size
+
     val result = when (numberOfIntersection) {
         0 -> "Вы не угадали ни одного числа"
         1 -> "Получите утешительный приз."
